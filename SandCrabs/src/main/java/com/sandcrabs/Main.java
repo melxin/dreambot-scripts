41cc54e94a2cb7acbf43012b433df26ba08b3b31
7ctx <7ctx@mail.com>
2021-05-18 18:12:03 +0200

    Add SandCrabs

---
+++ SandCrabs/src/main/java/com/sandcrabs/Main.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright (c) 2021, 7ctx <https://github.com/7ctx/> 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sandcrabs;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.List;
+import org.dreambot.api.Client;
+import org.dreambot.api.input.Mouse;
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.combat.Combat;
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.interactive.NPCs;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.SkillTracker;
+import org.dreambot.api.methods.skills.Skills;
+import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.listener.ChatListener;
+import org.dreambot.api.utilities.Timer;
+import org.dreambot.api.wrappers.interactive.NPC;
+import org.dreambot.api.wrappers.widgets.message.Message;
+
+@ScriptManifest(
+        author = "7ctx",
+        name = "SandCrabs_Afker",
+        version = 1.0,
+        description = "Afk at Sand Crabs, start script at tile between 3 Sand Crabs",
+        category = Category.THIEVING)
+
+public class Main extends SandCrabs implements ChatListener
+{
+    private Timer timer;
+    
+    /**
+     * Start script
+     */
+    @Override
+    public void onStart() 
+    {
+        super.onStart();
+                
+        timer = new Timer(); // Init timer
+
+        Client.getInstance().setMouseMovementAlgorithm(new WindMouse()); // Set custom mouse movement algorithm
+        
+        setFoodName("Tuna"); // food to use
+        setFoodAmount(27); // Food amount to take from bank
+        setEatFoodAtHP(Calculations.random(7, 12)); // Eat food at hp
+        setTrainingSkill(Skill.RANGED); // Set skill to train
+                 
+        SkillTracker.start(Skill.HITPOINTS);
+        SkillTracker.start(getTrainingSkill());
+        
+        log("Set afk tile: " + getLocalPlayer().getTile());
+        setAfkTile(getLocalPlayer().getTile()); // Set tile to afk at
+        
+        Area aggroArea = new Area(getAfkTile().getX() + 20, getAfkTile().getY(), getAfkTile().getX() + 30, getAfkTile().getY() + 5);
+        log("Set aggro area: " + aggroArea);
+        setAggroResetArea(aggroArea); // Set aggro area
+        
+        log("Initialized");
+        
+        log("Welcome to Sand Crabs Afker script by 7ctx.");
+        log("If you experience any issues while running this script please report them to me on the forums.");
+    }
+
+    /**
+     * Message received
+     *
+     * @param msg
+     */
+    @Override
+    public void onMessage(Message msg) 
+    {
+        if (msg.getMessage().contains("There is no ammo left in your quiver.")) 
+        {
+            log("There is no ammo left in your quiver.");
+            Walking.walkExact(new Tile(getLocalPlayer().getX(), getLocalPlayer().getY() + 10));
+            sleep(5000);
+            this.stop();
+        }
+    }
+
+    /**
+     * Is in area boolean
+     */
+    private boolean inArea(Area area) 
+    {
+        return area.contains(getLocalPlayer().getTile());
+    }
+
+    /**
+     * State enumeration
+     */
+    private enum State 
+    {
+        BANK, WALK_TO_CRABS, RESET_AGGRO, EAT_FOOD, SLEEP
+    };
+
+    /**
+     * State getter
+     * 
+     * @return State
+     */
+    private State getState() 
+    {
+        // Toggle auto retalitate on
+        if (!Combat.isAutoRetaliateOn()) 
+        {
+        log("Toggle auto retalitate to on");
+        Combat.toggleAutoRetaliate(true);          
+        }
+
+        // Bank
+        if (!Inventory.contains(Item -> Item != null && Item.getName().contains(getFoodName())))
+        {
+            log("Should bank..");
+            return State.BANK;
+        }
+
+        // Eat food here..
+        if (Skills.getBoostedLevels(Skill.HITPOINTS) <= this.getEatFoodAtHp()) 
+        {
+            log("Eat food: " + getFoodName() + " at hp: " + Skills.getBoostedLevels(Skill.HITPOINTS));
+            return State.EAT_FOOD;
+        }
+        
+        // Walk to afk tile
+        if (Inventory.contains(Item -> Item != null && Item.getName().contains(getFoodName()) && getLocalPlayer().distance(getAfkTile()) > 0 && !isResetAggro())) 
+        {
+         log("Walking to afk tile: " + getAfkTile());
+         return State.WALK_TO_CRABS;
+        }
+        
+        // Reset aggro
+        List<NPC> sandCrabs = NPCs.all(npc -> npc != null && npc.getName().equals("Sand Crab") && npc.distance(getLocalPlayer()) <= 1);
+        if (getLocalPlayer().distance(getAfkTile()) <= 1 || isResetAggro()) 
+        {
+        if (sandCrabs == null || sandCrabs.isEmpty()) 
+        {
+            log("Reset aggro");
+            return State.RESET_AGGRO;
+        }
+        }
+        
+        log("Sleeping..");
+        return State.SLEEP;
+    }
+
+    /**
+     * Loop
+     * 
+     * @return Calculations.random
+     */
+    @Override
+    public int onLoop() 
+    {
+        switch (getState()) 
+        {
+            case BANK:
+               super.doBanking();
+               break;
+
+            case WALK_TO_CRABS:
+                Walking.walkExact(getAfkTile());
+                sleepUntil(() -> getLocalPlayer().getTile() == getAfkTile(), Calculations.random(2000, 3000));
+                break;
+                
+            case RESET_AGGRO:
+                setResetAggro(true);
+                Walking.walk(getAggroResetArea().getRandomTile());
+                sleepUntil(() -> inArea(getAggroResetArea()), Calculations.random(4000, 5000));
+                if (inArea(getAggroResetArea())) 
+                {
+                    setResetAggro(false);
+                }
+                break;
+                
+            case EAT_FOOD:
+                Inventory.interact(item -> item != null && item.getName().contains("Tuna"), "Eat");
+                break;
+
+            case SLEEP:
+                if (Mouse.isMouseInScreen()) 
+                {
+                    Mouse.moveMouseOutsideScreen();
+                }
+                sleep(Calculations.random(2200, 3300));
+                break;
+        }
+        return Calculations.random(900, 1700);
+    }
+
+    /**
+     * Paint
+     * 
+     * @param g2 
+     */
+    @Override
+    public void onPaint(Graphics2D g2) 
+    {
+        g2.setColor(Color.BLACK);
+        g2.drawString("Runtime: " + timer.formatTime(), 10, 35);
+        g2.drawString("Hitpoints exp (p/h): " + SkillTracker.getGainedExperience(Skill.HITPOINTS) + "(" + SkillTracker.getGainedExperiencePerHour(Skill.HITPOINTS) + ")", 10, 65);
+        g2.drawString(getTrainingSkill().getName() + " exp (p/h): " + SkillTracker.getGainedExperience(getTrainingSkill()) + "(" + SkillTracker.getGainedExperiencePerHour(getTrainingSkill()) + ")", 10, 80);
+
+    }
+}
---
+++ SandCrabs/src/main/java/com/sandcrabs/SandCrabs.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2021, 7ctx <https://github.com/7ctx/> 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sandcrabs;
+
+import java.awt.Point;
+import org.dreambot.api.input.Mouse;
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.container.impl.bank.BankType;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.wrappers.interactive.Entity;
+
+public abstract class SandCrabs extends AbstractScript 
+{
+    private String foodName;
+    private int foodAmount;
+    private int eatFoodAtHP;
+    private Tile afkTile;
+    private Area aggroResetArea;
+    private boolean resetAggro;
+    private Skill trainingSkill;
+    
+    /**
+     * Set food name
+     * 
+     * @param foodName 
+     */
+    public void setFoodName(String foodName) 
+    {
+        this.foodName = foodName;
+    }
+    
+    /**
+     * Get food name
+     * 
+     * @return food name
+     */
+    public String getFoodName() 
+    {
+        return foodName;
+    }
+    
+    /**
+     * Set food amount, amount of food to take from bank
+     * 
+     * @param foodAmount 
+     */
+    public void setFoodAmount(int foodAmount) 
+    {
+        this.foodAmount = foodAmount;
+    }
+    
+    /**
+     * Get food amount
+     * 
+     * @return food amount
+     */
+    public int getFoodAmount() 
+    {
+        return foodAmount;
+    }
+    
+    /**
+     * Set eat food at HP, eat food at given integer
+     * 
+     * @param eatFoodAtHP 
+     */
+    public void setEatFoodAtHP(int eatFoodAtHP) 
+    {
+        this.eatFoodAtHP = eatFoodAtHP;
+    }
+    
+    /**
+     * Get eat food at HP
+     * 
+     * @return Integer at which to eat food
+     */
+    public int getEatFoodAtHp() 
+    {
+        return eatFoodAtHP;
+    }
+    
+    /**
+     * Set afk tile on start of the script, we keep returning here. should be tile inbetween 3 Sand Crabs
+     * 
+     * @param afkTile 
+     */
+    public void setAfkTile(Tile afkTile) 
+    {
+        this.afkTile = afkTile;
+    }
+    
+    /**
+     * Get afk tile
+     * 
+     * @return afk tile
+     */
+    public Tile getAfkTile() 
+    {
+        return afkTile;
+    }
+    
+    /**
+     * Set aggro reset are
+     * 
+     * @param aggroResetArea 
+     */
+    public void setAggroResetArea(Area aggroResetArea) 
+    {
+        this.aggroResetArea = aggroResetArea;
+    }
+    
+    /**
+     * Get aggro reset area
+     * 
+     * @return aggro reset area
+     */
+    public Area getAggroResetArea() 
+    {
+        return aggroResetArea;
+    }
+    
+    /**
+     * Set reset aggro boolean
+     * 
+     * @param setResetAggro 
+     */
+    public void setResetAggro(boolean setResetAggro) 
+    {
+        this.resetAggro = setResetAggro;
+    }
+    
+    /**
+     * isResetAggro boolean
+     * 
+     * @return resetAggro
+     */
+    public boolean isResetAggro() 
+    {
+        return resetAggro;
+    }
+    
+    /**
+     * Set skill to train
+     * 
+     * @param skill 
+     */
+    public void setTrainingSkill(Skill skill) 
+    {
+        this.trainingSkill = skill;
+    }
+    
+    /**
+     * Get skill
+     * 
+     * @return skill
+     */
+    public Skill getTrainingSkill() 
+    {
+        return trainingSkill;
+    }
+    
+    /**
+     * Handle Bank method
+     */
+    public void doBanking() 
+    {
+        Walking.walk(Bank.getClosestBankLocation().getCenter());
+        Entity bank = Bank.getClosestBank(BankType.CHEST);
+        if (bank == null) 
+        {
+            return;
+        }
+        sleepUntil(() -> bank.isOnScreen(), Calculations.random(2000, 3000));
+        bank.interactForceRight("Use");
+        log("STATUS = BANK");
+        sleepUntil(() -> Bank.open(), Calculations.random(3000, 4000));
+        //sleep(Calculations.random(2176, 2347));
+        //getBank().depositAllExcept(Constants.KNIFE);
+
+        if (Bank.isOpen()) 
+        {
+            if (!Bank.contains(Item -> Item != null && Item.getName().contains(getFoodName()))) 
+            {
+                log("Error: no food left in bank");
+                this.stop();
+                return;
+            }
+            sleep(Calculations.random(1200, 1900));
+            Bank.depositAllItems();
+            sleepUntil(() -> Inventory.emptySlotCount() >= getFoodAmount(), Calculations.random(4500, 7000));
+            Bank.withdraw(Item -> Item != null && Item.getName().contains(getFoodName()), getFoodAmount());
+            sleep(Calculations.random(1400, 1900));
+        }
+        
+        if (Inventory.count(Item -> Item != null && Item.getName().toLowerCase().contains(getFoodName())) == getFoodAmount())
+        {
+            Bank.close();
+            Mouse.move(new Point(Calculations.random(1, 763), Calculations.random(1, 500))); // Move mouse to random
+        }
+    }
+}
